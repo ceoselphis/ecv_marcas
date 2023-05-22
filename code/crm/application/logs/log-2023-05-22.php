8385a39530c0794c45022e428f2f0e1e929be430
Bryan Useche <bryan.useche.10@gmail.com>
2023-05-22 09:11:37 -0400

    Agregada aplicacion base

---
+++ code/crm/application/logs/log-2023-05-22.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-05-22 13:09:29 --> Severity: Warning --> include_once(C:\wamp64\www\ecv_marcas\code\crm\application\hooks./../libraries/App_modules.php): Failed to open stream: Permission denied C:\wamp64\www\ecv_marcas\code\crm\application\hooks\InitModules.php 12
+ERROR - 2023-05-22 13:09:29 --> Severity: Warning --> include_once(): Failed opening 'C:\wamp64\www\ecv_marcas\code\crm\application\hooks./../libraries/App_modules.php' for inclusion (include_path='.;C:\php\pear') C:\wamp64\www\ecv_marcas\code\crm\application\hooks\InitModules.php 12
+ERROR - 2023-05-22 13:09:29 --> Severity: Warning --> include_once(C:\wamp64\www\ecv_marcas\code\crm\application\hooks./../../system/helpers/directory_helper.php): Failed to open stream: Permission denied C:\wamp64\www\ecv_marcas\code\crm\application\hooks\InitModules.php 14
+ERROR - 2023-05-22 13:09:29 --> Severity: Warning --> include_once(): Failed opening 'C:\wamp64\www\ecv_marcas\code\crm\application\hooks./../../system/helpers/directory_helper.php' for inclusion (include_path='.;C:\php\pear') C:\wamp64\www\ecv_marcas\code\crm\application\hooks\InitModules.php 14
+ERROR - 2023-05-22 13:09:29 --> Severity: error --> Exception: Class "App_modules" not found C:\wamp64\www\ecv_marcas\code\crm\application\hooks\InitModules.php 16
